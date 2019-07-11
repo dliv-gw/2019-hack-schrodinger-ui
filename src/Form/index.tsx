9a9d3e0b823b82a00203143d1ccdf8e746e97c30
David Livingston <dlivingston@guidewire.com>
2019-07-11 15:51:01 -0500

    spinners

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Form, Input, Button, Alert } from 'antd';
+import { Form, Input, Button, Alert, Spin, Icon } from 'antd';
 import { FormComponentProps } from 'antd/lib/form/Form';
 
 import GetText from '../GetText';
@@ -76,205 +76,224 @@ class MainForm extends React.Component<IProps & FormComponentProps, IState> {
     const { error, isSubmitting, isUploadingLicense, isUploadingVin } = this.state;
 
     return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item label="VIN">
-          {getFieldDecorator('vin', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your vehicle's vin number`,
-              },
-            ],
-          })(<Input placeholder="VIN" allowClear={true} autoComplete="vin" />)}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          {isUploadingVin ? (
-            <GetText
-              docType="vin"
-              onVinSubmit={vin => {
-                this.setState(
-                  {
-                    isUploadingVin: false,
-                  },
-                  () => {
-                    if (vin) {
-                      setFieldsValue({
-                        vin,
-                      });
-                    }
-                  },
-                );
-              }}
-            />
-          ) : (
-            <Button
-              type="link"
-              htmlType="button"
-              disabled={isSubmitting}
-              onClick={() => {
-                this.setState({
-                  isUploadingVin: true,
-                });
-              }}
-            >
-              Upload Vehicle Vin
-            </Button>
-          )}
-        </Form.Item>
-        <Form.Item label="License Number">
-          {getFieldDecorator('licenseNumber', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your driver's license number`,
-              },
-            ],
-          })(
-            <Input
-              placeholder="Driver's License Number"
-              allowClear={true}
-              autoComplete="license-number"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          {isUploadingLicense ? (
-            <GetText
-              docType="license"
-              onLicenseSubmit={license => {
-                this.setState(
-                  {
-                    isUploadingLicense: false,
-                  },
-                  () => {
-                    if (license) {
-                      setFieldsValue({
-                        ...license,
-                      });
-                    }
-                  },
-                );
-              }}
-            />
-          ) : (
-            <Button
-              type="link"
-              htmlType="button"
-              disabled={isSubmitting}
-              onClick={() => {
-                this.setState({
-                  isUploadingLicense: true,
-                });
-              }}
-            >
-              Upload License
-            </Button>
-          )}
-        </Form.Item>
-        <Form.Item label="First Name">
-          {getFieldDecorator('fname', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your First Name`,
-              },
-            ],
-          })(<Input placeholder="First Name" allowClear={true} autoComplete="surname" />)}
-        </Form.Item>
-        <Form.Item label="Last Name">
-          {getFieldDecorator('lname', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your Last Name`,
-              },
-            ],
-          })(<Input placeholder="Last Name" allowClear={true} autoComplete="family-name" />)}
-        </Form.Item>
-        <Form.Item label="Date of Birth">
-          {getFieldDecorator('dob', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your Date of Birth`,
-              },
-            ],
-          })(
-            <Input
-              placeholder="Date of Birth as MM/DD/YYYY"
-              allowClear={true}
-              autoComplete="dob"
-            />,
-          )}
-        </Form.Item>
-        <Form.Item label="Address 1">
-          {getFieldDecorator('addressLine1', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your address`,
-              },
-            ],
-          })(<Input placeholder="Address" allowClear={true} autoComplete="address" />)}
-        </Form.Item>
-        <Form.Item label="City">
-          {getFieldDecorator('city', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your City`,
-              },
-            ],
-          })(<Input placeholder="City" allowClear={true} autoComplete="city" />)}
-        </Form.Item>
-        <Form.Item label="State">
-          {getFieldDecorator('state', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your state`,
-              },
-            ],
-          })(<Input placeholder="State" allowClear={true} autoComplete="state" />)}
-        </Form.Item>
-        <Form.Item label="Zip">
-          {getFieldDecorator('postalCode', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your Zip Code`,
-              },
-            ],
-          })(<Input placeholder="5 digit Zip Code" allowClear={true} autoComplete="zip" />)}
-        </Form.Item>
-        <Form.Item label="Country">
-          {getFieldDecorator('country', {
-            rules: [
-              {
-                required: true,
-                message: `Please provide your 2 digit Country`,
-              },
-            ],
-          })(<Input placeholder="2 letter Country" allowClear={true} autoComplete="zip" />)}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit" disabled={isSubmitting}>
-            Get Quote
-          </Button>
-        </Form.Item>
-        {error && (
-          <Form.Item {...tailFormItemLayout}>
-            <Alert
-              message="Error Getting Quote"
-              description={
-                <pre>
-                  <code>{error.message || JSON.stringify(error, null, 2)}</code>
-                </pre>
-              }
-            />
-          </Form.Item>
+      <div>
+        {isSubmitting && (
+          <div
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              background: 'rgba(0, 0, 0, 0.2)',
+              zIndex: 1000 * 1000,
+            }}
+          >
+            <Spin tip="Retrieving your quote">
+              <div style={{ width: '100vw', height: '100vh' }}> </div>
+            </Spin>
+          </div>
         )}
-      </Form>
+        <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+          <fieldset disabled={isSubmitting}>
+            <Form.Item label="VIN">
+              {getFieldDecorator('vin', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your vehicle's vin number`,
+                  },
+                ],
+              })(<Input placeholder="VIN" allowClear={true} autoComplete="vin" />)}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              {isUploadingVin ? (
+                <GetText
+                  docType="vin"
+                  onVinSubmit={vin => {
+                    this.setState(
+                      {
+                        isUploadingVin: false,
+                      },
+                      () => {
+                        if (vin) {
+                          setFieldsValue({
+                            vin,
+                          });
+                        }
+                      },
+                    );
+                  }}
+                />
+              ) : (
+                <Button
+                  type="link"
+                  htmlType="button"
+                  disabled={isSubmitting}
+                  onClick={() => {
+                    this.setState({
+                      isUploadingVin: true,
+                    });
+                  }}
+                >
+                  Upload Vehicle Vin
+                </Button>
+              )}
+            </Form.Item>
+            <Form.Item label="License Number">
+              {getFieldDecorator('licenseNumber', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your driver's license number`,
+                  },
+                ],
+              })(
+                <Input
+                  placeholder="Driver's License Number"
+                  allowClear={true}
+                  autoComplete="license-number"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              {isUploadingLicense ? (
+                <GetText
+                  docType="license"
+                  onLicenseSubmit={license => {
+                    this.setState(
+                      {
+                        isUploadingLicense: false,
+                      },
+                      () => {
+                        if (license) {
+                          setFieldsValue({
+                            ...license,
+                          });
+                        }
+                      },
+                    );
+                  }}
+                />
+              ) : (
+                <Button
+                  type="link"
+                  htmlType="button"
+                  disabled={isSubmitting}
+                  onClick={() => {
+                    this.setState({
+                      isUploadingLicense: true,
+                    });
+                  }}
+                >
+                  Upload License
+                </Button>
+              )}
+            </Form.Item>
+            <Form.Item label="First Name">
+              {getFieldDecorator('fname', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your First Name`,
+                  },
+                ],
+              })(<Input placeholder="First Name" allowClear={true} autoComplete="surname" />)}
+            </Form.Item>
+            <Form.Item label="Last Name">
+              {getFieldDecorator('lname', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your Last Name`,
+                  },
+                ],
+              })(<Input placeholder="Last Name" allowClear={true} autoComplete="family-name" />)}
+            </Form.Item>
+            <Form.Item label="Date of Birth">
+              {getFieldDecorator('dob', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your Date of Birth`,
+                  },
+                ],
+              })(
+                <Input
+                  placeholder="Date of Birth as MM/DD/YYYY"
+                  allowClear={true}
+                  autoComplete="dob"
+                />,
+              )}
+            </Form.Item>
+            <Form.Item label="Address 1">
+              {getFieldDecorator('addressLine1', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your address`,
+                  },
+                ],
+              })(<Input placeholder="Address" allowClear={true} autoComplete="address" />)}
+            </Form.Item>
+            <Form.Item label="City">
+              {getFieldDecorator('city', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your City`,
+                  },
+                ],
+              })(<Input placeholder="City" allowClear={true} autoComplete="city" />)}
+            </Form.Item>
+            <Form.Item label="State">
+              {getFieldDecorator('state', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your state`,
+                  },
+                ],
+              })(<Input placeholder="State" allowClear={true} autoComplete="state" />)}
+            </Form.Item>
+            <Form.Item label="Zip">
+              {getFieldDecorator('postalCode', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your Zip Code`,
+                  },
+                ],
+              })(<Input placeholder="5 digit Zip Code" allowClear={true} autoComplete="zip" />)}
+            </Form.Item>
+            <Form.Item label="Country">
+              {getFieldDecorator('country', {
+                rules: [
+                  {
+                    required: true,
+                    message: `Please provide your 2 digit Country`,
+                  },
+                ],
+              })(<Input placeholder="2 letter Country" allowClear={true} autoComplete="zip" />)}
+            </Form.Item>
+          </fieldset>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit" disabled={isSubmitting}>
+              {isSubmitting && <Icon type="loading" />} Get Quote
+            </Button>
+          </Form.Item>
+          {error && (
+            <Form.Item {...tailFormItemLayout}>
+              <Alert
+                message="Error Getting Quote"
+                description={
+                  <pre>
+                    <code>{error.message || JSON.stringify(error, null, 2)}</code>
+                  </pre>
+                }
+              />
+            </Form.Item>
+          )}
+        </Form>
+      </div>
     );
   }
 }
